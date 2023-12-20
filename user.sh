2700af299c54722b2dcf2530c4b3ff361079e0b4
NagaYara <yrnp2355@gmail.com>
2023-12-20 18:14:55 +0530

    user app creation

---
+++ user.sh
@@ -0,0 +1,82 @@
+#!/bin/bash 
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MONGODB_HOST=mongodb.ikart.online
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N"
+    exit 1 # you can give other than 0
+fi
+
+dnf module disable nodejs -y &>> $LOGFILE
+VALIDATE $? "Disabling current NodeJS"
+
+dnf module enable nodejs:18 -y  &>> $LOGFILE
+VALIDATE $? "Disabling current NodeJS"
+
+dnf install nodejs -y  &>> $LOGFILE
+VALIDATE $? "Installing NodeJS:18"
+
+id roboshop # if roboshop user does not exist, then it is failure, so that using if condition
+if [ $? -ne 0 ]
+then
+    useradd roboshop
+    VALIDATE $? "roboshop user creation"
+else
+    echo "roboshop user already exists $Y Skipping $N"
+fi
+
+mkdir -p /app
+VALIDATE $? "creating app directory"
+
+curl -L -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip &>> $LOGFILE
+VALIDATE $? "Downloading user application"
+
+cd /app
+
+unzip -o /tmp/user.zip  &>> $LOGFILE
+VALIDATE $? "unzipping user"
+
+npm install  &>> $LOGFILE
+VALIDATE $? "Installing dependencies"
+
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service &>> $LOGFILE
+VALIDATE $? "Copying user service file"
+
+systemctl daemon-reload &>> $LOGFILE
+VALIDATE $? "daemon reload"
+
+systemctl enable user &>> $LOGFILE
+VALIDATE $? "Enabling user"
+
+systemctl start user &>> $LOGFILE
+VALIDATE $? "Starting user"
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/systemd/system/umongo.repo &>> $LOGFILE
+VALIDATE $? "Copying MongoDB repo"
+
+dnf install mongodb-org-shell -y &>> $LOGFILE
+VALIDATE $? "Installing mongodb client"
+
+mongo --host $MONGODB_HOST </app/schema/user.js &>> $LOGFILE
+VALIDATE $? "Load user data into MongoDB"
